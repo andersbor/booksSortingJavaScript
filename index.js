7a03de62ebdcf1860895eab10654d8cea95492f1
Anders Børjesson <anbo@zealand.dk>
2022-10-12 18:48:39 +0200

    two bug fixes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const baseUrl = "http://localhost:49687/api/books"
+// const baseUrl = "https://anbo-restbooks.azurewebsites.net/api/books" 
+
 
 Vue.createApp({
     data() {
         return {
+            allBooks: [],
             books: [],
             title: null
         }
@@ -14,29 +17,32 @@ Vue.createApp({
         async getAll(url) {
             try {
                 const response = await axios.get(url)
-                this.books = await response.data
-                console.log(this.books)
+                this.allBooks = await response.data
+                this.books = this.allBooks
+                console.log(this.allBooks)
             } catch (ex) {
                 alert(ex.message) // https://www.w3schools.com/js/js_popup.asp
             }
         },
         sortById() {
             // https://www.w3schools.com/js/js_array_sort.asp
-            this.books.sort((book1, book2) => book1.id - book2.id)
+            this.allBooks.sort((book1, book2) => book1.id - book2.id)
         },
         sortByTitle() {
-            this.books.sort((book1, book2) => book1.title.localeCompare(book2.title))
+            this.allBooks.sort((book1, book2) =>
+                book1.title.localeCompare(book2.title))
         },
         sortByPriceAscending() {
-            this.books.sort((book1, book2) => book1.price - book2.price)
+            this.allBooks.sort((book1, book2) => book1.price - book2.price)
         },
         sortByPriceDescending() {
-            this.books.sort((book1, book2) => book2.price - book1.price)
+            this.allBooks.sort((book1, book2) => book2.price - book1.price)
         },
         filterByTitle(title) {
-            console.log("Title: " + title)
-            this.books = this.books.filter(b => b.title.includes(title))
-            console.log("Books: " + this.books)
+            console.log("Title:" + title + ":")
+            console.log("All books " + this.allBooks)
+            this.books = this.allBooks.filter(b => b.title.includes(title))
+            console.log("filtered Books: " + this.books)
         }
     }
 }).mount("#app")
